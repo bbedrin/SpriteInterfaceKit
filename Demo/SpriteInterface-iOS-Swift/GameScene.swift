f0a82bd4ff7101892c12d6ce7d9ae151181d3514
bbedrin <brett.bedrin@kaplan.com>
2015-09-11 00:29:52 -0400

    Adjusted project structure and replaced targets

---
+++ Demo/SpriteInterface-iOS-Swift/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  SpriteInterface-iOS-Swift
+//
+//  Created by Brett Bedrin on 9/11/15.
+//  Copyright (c) 2015 Brett Bedrin. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+    
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
